e4616cf09988cdb925cf7f60c2cf7f32e32a6ff3
EduardoBelardinuci <belardinucieduardo@gmail.com>
2023-04-26 15:39:39 -0300

    Integração BD e Produto

--- WebApp_Tarde/Controllers/ProdutosController.cs
+++ WebApp_Tarde/Controllers/ProdutosController.cs
@@ -4,9 +4,16 @@ namespace WebApp_Tarde.Controllers
 {
     public class ProdutosController : Controller
     {
+        private readonly Contexto db;
+
+        public ProdutosController(Contexto opt)
+        {
+            db = opt;
+        }
+
         public IActionResult ListaP()
         {
-            return View();
+            return View(db.Produtos.ToList() );
         }
         public IActionResult CadastroP()
         {
