b433853865c5e347191364bce9efaac7290e782e
EduardoBelardinuci <belardinucieduardo@gmail.com>
2023-05-17 15:58:55 -0300

    Vendas e ItensVenda

---
+++ WebApp_Tarde/Controllers/VendasController.cs
@@ -0,0 +1,31 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using WebApp_Tarde.Entidades;
+using WebApp_Tarde.Models;
+
+namespace WebApp_Tarde.Controllers
+{
+    public class VendasController : Controller
+    {
+        private Contexto db;
+        public VendasController(Contexto contexto)
+        {
+            db = contexto;
+        }
+        public IActionResult Lista()
+        {
+            NovaVendaViewModel model = new NovaVendaViewModel();
+            model.Lista_Produtos = db.Produtos.ToList();
+            return View(model);
+        }
+
+        public IActionResult Cadastro()
+        {
+            return View();
+        }
+        [HttpPost]
+        public IActionResult SalvarDados(VendasEntidade dados)
+        {
+            return RedirectToAction("Lista");
+        }
+    }
+}
